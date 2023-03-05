d121de4e0c59b6fcafb37bea02b27b14b806e3c4
Simerus Mahesh <63352151+SimerusM@users.noreply.github.com>
2023-03-04 19:24:18 -0500

    Update sanityClient.js

--- opensea-clone/lib/sanityClient.js
+++ opensea-clone/lib/sanityClient.js
@@ -4,7 +4,7 @@ export const client = sanityClient({
     projectId: '4dsdt2ky',
     dataset: 'production',
     apiVersion: '2021-03-25',
-    token: 'skFpIuegowZn41khyn34YCYSyqIujBVpo07f3JzilEfpcHS1TchMWaqJen6zG2j3wdU7H826G3JYhEa1OSwpTemO19MY1eBaH7mXBItJrqwescgdCtxDwgJhTQbm8ldez4ruFZn5LcJxZ1ZbLwqSORJQY3lyJxU9IDqg4tOAUdQiPByyFd9M',
+    token: '',
     useCdn: false,
 
 })
